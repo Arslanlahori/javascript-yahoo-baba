469a0b6eb229e6ebbd405230756265ba16f9109c
arslanlahori <arslanansari786.iu@gmail.com>
2022-08-08 22:11:41 +0500

    this is if,if else, if else if statements

---
+++ ifstatement.js
@@ -0,0 +1,29 @@
+var a = 10;
+var b = 20;
+if (a < b) {
+    console.log("yes a is greater");
+}
+// && ||
+if (a == b || a < b) {
+    console.log("both are correct")
+}
+
+//if else
+if (a > b) {
+    console.log("a is greater");
+}
+else {
+    console.log("b is greater");
+}
+
+// if else if
+if (a > b) {
+    console.log("a is greater");
+}
+else if (a > b) {
+    console.log("b is greater");
+}
+else {
+    console.log("noting");
+}
+
