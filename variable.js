cf89cf279eab026200349a15a5ec540ff2252009
arslanlahori <arslanansari786.iu@gmail.com>
2022-08-08 22:13:25 +0500

    hoe to declare variable in javascript

---
+++ variable.js
@@ -0,0 +1,17 @@
+// in this i declare variables
+
+
+var a = "Here i am initialize a variable in javascript";
+document.write(a);
+var w, v, x;
+x = 2;
+v = 3;
+w = v + x;
+document.write(w);
+
+// there 3 type of variable in javascript
+//1. var
+//2. let-> it is same as var but can't be redeclare
+// for example let a=7;
+// let a=9; it is wrong
+//3. const -> it is for const value, a value which is not change
